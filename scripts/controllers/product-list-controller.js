5395287eb53c209d0139dfc5de4af0bb0d7c6b79
Jones V Rajan <Jones V Rajan>
2016-10-02 17:58:35 +0530

    Grand total added.

--- scripts/controllers/product-list-controller.js
+++ scripts/controllers/product-list-controller.js
@@ -10,12 +10,29 @@
 angular.module('angularShoppingModuleApp')
   .controller('productListController', ['$http', function($http) {
   	var self = this;
+  	self.grandtotal = 0;
   	self.productlist = [];
-  	self.quantity = 1;
+  	
 	$http.get('scripts/cart.json').then(function(response) {
 		self.productlist = response.data.productsInCart;
 		}, function(errResponse) {
 		console.error('Error while fetching products');
 	});
+
+	self.totals = [0];
+
+	self.total = function(index) {
+  		var totalprice = index.p_originalprice * index.p_quantity;
+  		var grandtotals = self.grandtotal + totalprice;
+  		self.totals.push(grandtotals);
+ 		return grandtotals;
+    };
+
+    self.grandtotale = function() {
+    	if (self.totals.length > 0 ) {
+			var grandtotaleno = self.totals.reduce(function (a, b) { return a + b; });
+		};
+		return grandtotaleno;
+	}
 
   }]);
