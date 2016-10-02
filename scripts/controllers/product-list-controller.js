271361978221a6c169cf8c8b79fd8c6a7b30c1c9
Jones V Rajan <Jones V Rajan>
2016-10-02 12:13:12 +0530

    Refactoring code.

--- scripts/controllers/product-list-controller.js
+++ scripts/controllers/product-list-controller.js
@@ -14,22 +14,8 @@ angular.module('angularShoppingModuleApp')
   	self.quantity = 1;
 	$http.get('scripts/cart.json').then(function(response) {
 		self.productlist = response.data.productsInCart;
-		console.log(self.productlist);
 		}, function(errResponse) {
 		console.error('Error while fetching products');
 	});
-
-	self.addtoCart = function() {
-        alert(self.productlist[0].p_quantity);
-    }
-
-	self.grandTotal = function() {
-        var total = 0;
-        angular.forEach(self.productlist[0], function(item) {
-          total = self.productlist.p_originalprice * self.productlist.p_quantity;
-        })
-
-        return total;
-    }
 
   }]);
